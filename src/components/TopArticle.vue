e21aa606885f94e08b58440d62afaf736fd3f181
jiajiecheng <2403428097@qq.com>
2022-09-07 09:37:09 +0800

    feat: 完成热门文章界面

---
+++ src/components/TopArticle.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="TopArticleContainer">
+    <img :src="imgUrl" alt="">
+    <div class="content">
+      <span>{{label}}</span>
+      <h1>{{title}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopArticle',
+  props: ['imgUrl','label','title']
+}
+</script>
+
+<style scoped>
+.TopArticleContainer {
+  display: flex;
+  justify-content: center;
+  position: relative;
+  width: 100%;
+  height: 230px;
+  margin-bottom: 10px;
+}
+.TopArticleContainer img {
+  width: 100%;
+  height: 100%;
+}
+.content {
+  width: 100%;
+  bottom: 0;
+  position: absolute;
+}
+.content span {
+  background-color: rgba(255, 255, 255, 0.8);
+  padding: 4px;
+  font-size: 12px;
+}
+.content span,
+.content h1 {
+  margin-left: 10px;
+}
+.content h1 {
+  color: #fff;
+  font-size: 20px;
+}
+</style>
--- src/views/Information.vue
+++ src/views/Information.vue
@@ -45,6 +45,9 @@
       <div class="advertising">
         <div class="hot">
           <h1 class="title">热门文章</h1>
+          <div v-for="article in topArticle " :key="article.id">
+            <TopArticle :imgUrl="article.imgSrc" :label="article.lable" :title="article.title"/>
+          </div>
         </div>
       </div>
     </div>
@@ -52,58 +55,64 @@
 </template>
 
 <script>
+import TopArticle from '@/components/TopArticle.vue'
 export default {
   name: 'Information',
   data() {
     return {
-      articleLists:[
+      articleLists: [
         {
-          id:1,
-          imgSrc:'http://www.chi1dun.com/blog/wp-content/uploads/2020/08/pexels-sebastiaan-stam-1482476.jpg',
-          title:'厨房人员配备方案该如何安排才能提高出餐效率',
-          date:'10月 10, 2020',
-          abstract:`餐厅厨房工作人员怎样装备？厨房人员配备方案就有很多依据。
+          id: 1,
+          imgSrc: 'http://www.chi1dun.com/blog/wp-content/uploads/2020/08/pexels-sebastiaan-stam-1482476.jpg',
+          title: '厨房人员配备方案该如何安排才能提高出餐效率',
+          date: '10月 10, 2020',
+          abstract: `餐厅厨房工作人员怎样装备？厨房人员配备方案就有很多依据。
             很多出资者在开了一家低本钱的出资餐厅后会面临人员安排。
             很多餐厅不重视后厨房的人员配备，但后厨房的人员是整个餐厅的中心。
             因为厨房人员、规划、档次的不同，咱们需求的餐厅人员装备也不同。
             那么，厨房人员岗位职责细分该如何实现，咱们应该如何分配餐厅职工呢`
         },
         {
-          id:2,
-          imgSrc:'http://www.chi1dun.com/blog/wp-content/uploads/2020/08/pexels-sebastiaan-stam-1482476.jpg',
-          title:'厨房人员配备方案该如何安排才能提高出餐效率',
-          date:'10月 10, 2020',
-          abstract:`餐厅厨房工作人员怎样装备？厨房人员配备方案就有很多依据。
+          id: 2,
+          imgSrc: 'http://www.chi1dun.com/blog/wp-content/uploads/2020/08/pexels-sebastiaan-stam-1482476.jpg',
+          title: '厨房人员配备方案该如何安排才能提高出餐效率',
+          date: '10月 10, 2020',
+          abstract: `餐厅厨房工作人员怎样装备？厨房人员配备方案就有很多依据。
             很多出资者在开了一家低本钱的出资餐厅后会面临人员安排。
             很多餐厅不重视后厨房的人员配备，但后厨房的人员是整个餐厅的中心。
             因为厨房人员、规划、档次的不同，咱们需求的餐厅人员装备也不同。
             那么，厨房人员岗位职责细分该如何实现，咱们应该如何分配餐厅职工呢`
         },
         {
-          id:3,
-          imgSrc:'http://www.chi1dun.com/blog/wp-content/uploads/2020/08/pexels-sebastiaan-stam-1482476.jpg',
-          title:'厨房人员配备方案该如何安排才能提高出餐效率',
-          date:'10月 10, 2020',
-          abstract:`餐厅厨房工作人员怎样装备？厨房人员配备方案就有很多依据。
+          id: 3,
+          imgSrc: 'http://www.chi1dun.com/blog/wp-content/uploads/2020/08/pexels-sebastiaan-stam-1482476.jpg',
+          title: '厨房人员配备方案该如何安排才能提高出餐效率',
+          date: '10月 10, 2020',
+          abstract: `餐厅厨房工作人员怎样装备？厨房人员配备方案就有很多依据。
             很多出资者在开了一家低本钱的出资餐厅后会面临人员安排。
             很多餐厅不重视后厨房的人员配备，但后厨房的人员是整个餐厅的中心。
             因为厨房人员、规划、档次的不同，咱们需求的餐厅人员装备也不同。
             那么，厨房人员岗位职责细分该如何实现，咱们应该如何分配餐厅职工呢`
         },
         {
-          id:4,
-          imgSrc:'http://www.chi1dun.com/blog/wp-content/uploads/2020/08/pexels-sebastiaan-stam-1482476.jpg',
-          title:'厨房人员配备方案该如何安排才能提高出餐效率',
-          date:'10月 10, 2020',
-          abstract:`餐厅厨房工作人员怎样装备？厨房人员配备方案就有很多依据。
+          id: 4,
+          imgSrc: 'http://www.chi1dun.com/blog/wp-content/uploads/2020/08/pexels-sebastiaan-stam-1482476.jpg',
+          title: '厨房人员配备方案该如何安排才能提高出餐效率',
+          date: '10月 10, 2020',
+          abstract: `餐厅厨房工作人员怎样装备？厨房人员配备方案就有很多依据。
             很多出资者在开了一家低本钱的出资餐厅后会面临人员安排。
             很多餐厅不重视后厨房的人员配备，但后厨房的人员是整个餐厅的中心。
             因为厨房人员、规划、档次的不同，咱们需求的餐厅人员装备也不同。
             那么，厨房人员岗位职责细分该如何实现，咱们应该如何分配餐厅职工呢`
         }
+      ],
+      topArticle: [
+        { id: 1, imgSrc: 'http://www.chi1dun.com/blog/wp-content/uploads/2020/08/pexels-sebastiaan-stam-1482476.jpg', lable: '商家活动', title: '商家活动的标题' },
+        { id: 2, imgSrc: 'http://www.chi1dun.com/blog/wp-content/uploads/2020/08/pexels-sebastiaan-stam-1482476.jpg', lable: '商家活动', title: '商家活动的标题' }
       ]
     }
-  }
+  },
+  components: { TopArticle }
 }
 </script>
 
@@ -188,12 +197,22 @@ export default {
 
 /*  */
 .advertising {
-  width: 480px;
+  width: 380px;
+  padding-left: 20px;
   display: flex;
-  flex-direction: row;
-  background-color: blue;
+  flex-direction: column;
+  /* background-color: blue; */
 }
-.hot .title{
-  font-size: 18px;
+.hot {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+}
+.hot .title {
+  font-size: 20px;
+  margin-top: 0;
+  margin-bottom: 8px;
+  /* margin: 0; */
 }
 </style>
